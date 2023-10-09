8e1acd07665622bfb8c7c9a67a22bdc817913985
TDumler74 <tatjana.dumler@gmail.com>
2023-10-09 10:58:11 +0200

    HW-Lesson-02

---
+++ lesson02/TD_Hello_Web/src/main/java/td_hello_web/TdHelloWebApplication.java
@@ -0,0 +1,13 @@
+package td_hello_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdHelloWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdHelloWebApplication.class, args);
+    }
+
+}
